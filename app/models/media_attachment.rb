3c33729b4779d69f9265e2c75f06ee79efc76003
幻葬咲夜 <zyqwert2@gmail.com>
2020-07-16 14:11:45 +0800

    remove the compression of image attachments

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -59,7 +59,7 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 1_638_400, # 1280x1280px
+      # pixels: 1_638_400, # 1280x1280px
       file_geometry_parser: FastGeometryParser,
     }.freeze,
 
